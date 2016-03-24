7cec262ef1b2ed9ae73df4339f87623daf9036c6
OnlyCZY <853852998@qq.com>
2016-03-24 14:50:02 +0800

    add webview pue

--- app/src/main/java/com/buoyantec/eagle_android/ui/activity/Pue.java
+++ app/src/main/java/com/buoyantec/eagle_android/ui/activity/Pue.java
@@ -3,19 +3,27 @@ package com.buoyantec.eagle_android.ui.activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.AbsoluteLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class Pue extends BaseActivity {
     private Toolbar toolbar;
     private TextView subToolbarTitle;
+    private WebView webView;
+    private ProgressBar progressBar;
 
     @Override
     protected void initView(Bundle savedInstanceState) {
         setContentView(R.layout.activity_pue);
         toolbar = getViewById(R.id.sub_toolbar);
         subToolbarTitle = getViewById(R.id.sub_toolbar_title);
+        webView = getViewById(R.id.pue_web);
+        progressBar = getViewById(R.id.pue_progress);
     }
 
     @Override
@@ -26,6 +34,27 @@ public class Pue extends BaseActivity {
     @Override
     protected void processLogic(Bundle savedInstanceState) {
         initToolbar();
+        // 加载web
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webView.loadUrl("http://www.buoyantec.com");
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                // TODO Auto-generated method stub
+                if (newProgress == 100) {
+                    // 网页加载完成
+                    progressBar.setVisibility(View.GONE);
+                } else {
+                    // 加载中
+                    if (View.GONE == progressBar.getVisibility()) {
+                        progressBar.setVisibility(View.VISIBLE);
+                    }
+                    progressBar.setProgress(newProgress);
+                }
+                super.onProgressChanged(view, newProgress);
+            }
+        });
     }
 
     private void initToolbar() {
